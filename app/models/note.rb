3e1d28547b765e88ce4ce3eae13134b8bd75c406
Gonzalo Messina <gonzalo.messina@widergy.com>
2026-02-19 12:45:04 -0300

    Add word count method

--- app/models/note.rb
+++ app/models/note.rb
@@ -4,19 +4,23 @@
 #
 #  id         :bigint(8)        not null, primary key
 #  title      :string
-#  content    :text
+#  content    :string
 #  note_type  :integer
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  user_id    :bigint(8)        not null
-#
 
 class Note < ApplicationRecord
-  enum note_type: { review:   0,
-                    comment:  1 
-                  }
+  enum note_type: { review: 0,
+                    critique: 1 }
 
   validates :title, :content, :note_type, :user_id, presence: true
 
   belongs_to :user
+
+  def self.word_count(note_id)
+    note = find(note_id)
+    count = note.content.split.size
+    Rails.logger.debug "The note has #{count} words."
+  end
 end
